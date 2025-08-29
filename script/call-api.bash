def12a8bd9503f4d67ddcd2f9b5fcc68cfc8f61c
suisan0731 <suisan0731@gmail.com>
2025-08-29 11:32:14 +0900

    modify

--- script/call-api.bash
+++ script/call-api.bash
@@ -5,7 +5,7 @@ curl -v -X POST https://api.line.me/v2/bot/message/push \
 -H 'Content-Type: application/json' \
 -H 'Authorization: Bearer line_access_token' \
 -d '{
-    "to": "groupId"
+    "to": "groupId",
     "messages":[
         {
             "type":"text",
